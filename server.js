24c92406f323fd29fb117b31bde832b61bcc11f7
pragun <pragungandotra7@gmail.com>
2021-01-30 22:29:24 +0530

    prepared for deployment

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ app.use(express.json({ extended: false }));
 //Define Routes
 app.use("/api/", require("./routes/jobs"));
 
+//Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+    // Set static folder
+    app.use(express.static("client/build"));
+  
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT,()=>console.log(`server started on port ${PORT}`));
